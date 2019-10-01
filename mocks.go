01bf1358c5395e7d3b330b985bc1a4b3c0accc97
ervitis <vitomarti@gmail.com>
2019-10-01 20:40:03 +0200

    fix: deleted unused functions

--- mocks.go
+++ mocks.go
@@ -12,13 +12,3 @@ func (m *mockValidaktor) initializeValidators(tags ...string) map[string]validat
 	args := m.Called(tags)
 	return args.Get(0).(map[string]validator)
 }
-
-func (m *mockValidaktor) getValidator(tag string) validator {
-	args := m.Called(0)
-	return args.Get(0).(validator)
-}
-
-func (m *mockValidaktor) ValidateData(s interface{}) []error {
-	args := m.Called(0)
-	return args.Get(0).([]error)
-}
